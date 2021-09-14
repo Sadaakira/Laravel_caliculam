137906d751322cb9501824d18fc6367aecff8884
Sadaakira <satomizu0804@gmail.com>
2021-09-14 21:17:21 +0900

    課題2：ブログ投稿の削除処理実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
+//use Illuminate\Http\Request;
+
 
 class PostController extends Controller
 {
@@ -15,5 +17,36 @@ class PostController extends Controller
     public function show(Post $post)
     {
         return view('show')->with(['post' => $post]);
+    }
+    
+    public function create()
+    {
+        return view('create');
+    }
+    
+    public function store(PostRequest $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
     }
 }
